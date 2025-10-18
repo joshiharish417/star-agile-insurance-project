a39a841f4a172d8c683ec9ae50a5bc0da69ca6c5
joshiharish417 <joshiharish417@gmail.com>
2025-10-18 14:56:05 +0530

    Set ChromeDriver path in UI test setup

--- src/test/java/com/project/staragile/insureme/UI/InsureMeUITest.java
+++ src/test/java/com/project/staragile/insureme/UI/InsureMeUITest.java
@@ -26,6 +26,7 @@ public class InsureMeUITest {
     @BeforeAll
     public static void setup() {
         System.setProperty("selenium.manager.log.level", "TRACE");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
